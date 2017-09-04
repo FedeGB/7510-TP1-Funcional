2143ad96443ab09cd82605a95c11ce26ae1fe95d
Federico Garcia Brion <fedeagb@gmail.com>
2017-09-04 00:37:55 -0300

    Added some base code. Will be refactored later.

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,44 @@
 (ns logical-interpreter)
+(require '[clojure.string :as str])
+
+(defn isValidRule
+  [entry]
+  (not (nil? (re-find #"^[^\(]*\([^)]*\) :- [^\(]*\([^)]*\), [^\(]*\([^)]*\)$" entry)))
+)
+
+(defn isValidFact
+  [entry]
+  (not (nil? (re-find #"^[^\(]*\([^)]*\)$" entry)))
+)
+
+(defn processValidFact
+  [fact factMap]
+  (def factMap {})
+  ;(assoc factMap {(get fact 0) (get fact 1)})
+
+)
+
+(defn operateElement
+  [element]
+  (if (isValidRule element)
+    (def case (str/split element #"(:-)"))
+  )
+  (if (isValidFact element)
+    (def case (str/split element #"(\(|\))"))
+  )
+  (if-not (or (isValidFact element) (isValidRule element))
+    (def case nil)
+  )
+  case
+)
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-  nil)
+  (def inputParsed (str/split (str/replace parent-database #"(\t|\n)" "") #"\."))
+  (for [element inputParsed]
+    (println (operateElement element))
+  )
+  nil
+)
